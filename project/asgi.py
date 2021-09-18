f99eca32e38c4dc82e37eb314f0c7235803ed767
osama614 <os.khaled2020@gmail.com>
2021-09-18 15:18:58 +0200

    modify asgi middleware 2

--- project/asgi.py
+++ project/asgi.py
@@ -8,16 +8,19 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
+import django
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+#from .middleware.channelsmiddleware import JwtAuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from notification.routing import websocket_urlpatterns
-from channels.security.websocket import AllowedHostsOriginValidator
-from .middleware.channelsmiddleware import JwtAuthMiddlewareStack
+
 
 #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.development')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.production')
-
+django.setup()
+from .middleware.channelsmiddleware import JwtAuthMiddlewareStack
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
