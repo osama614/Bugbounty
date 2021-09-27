88462de49bad4b2755c603f1e4798480cc7ef195
osama614 <os.khaled2020@gmail.com>
2021-09-28 00:44:14 +0200

    fixing ip address

--- project/middleware/ip_middleware.py
+++ project/middleware/ip_middleware.py
@@ -3,5 +3,5 @@ from django.utils.deprecation import MiddlewareMixin
 class RemoteAddrMiddleware(MiddlewareMixin):
     def process_request(self, request):
         if 'HTTP_X_FORWARDED_FOR' in request.META:
-            ip = request.META['HTTP_X_FORWARDED_FOR'].split(',')[0].strip()
+            ip = request.META['HTTP_X_FORWARDED_FOR'].split(',')[-1].strip()
             request.META['REMOTE_ADDR'] = ip
